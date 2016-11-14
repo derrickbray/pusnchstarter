8ee89a6d06e5972cfc7fcdf447d8bf10ce127703
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-14 11:39:29 -0600

    Loop through projects

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        name: 'Fancy Iced Coffee',
+        description: 'Isaac probably needs one of these',
+        goal: 1000000,
+        earned: 20000,
+      },
+      {
+        name: 'Cat Sticker Book',
+        description: 'Because why not?',
+        goal: 5000,
+        earned: 100000,
+      },
+      {
+        name: 'Robot Trashcans',
+        description: 'This changes EVERYTHING',
+        goal: 100000,
+        earned: 250000,
+      },
+      {
+        name: 'Potato Salad',
+        description: 'Why did you spend money on this?',
+        goal: 5,
+        earned: 100000000,
+      },
+    ];
+  }
 });
