7e70d7efe430add08f27a8461d4655d2d1dfcdc0
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-15 09:51:23 -0600

    feat: Fetch real data from server

--- app/index/route.js
+++ app/index/route.js
@@ -2,31 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        name: 'Fancy Iced Coffee',
-        description: 'Isaac probably needs one of these',
-        goal: 1000000,
-        earned: 20000,
-      },
-      {
-        name: 'Cat Sticker Book',
-        description: 'Because why not?',
-        goal: 5000,
-        earned: 100000,
-      },
-      {
-        name: 'Robot Trashcans',
-        description: 'This changes EVERYTHING',
-        goal: 100000,
-        earned: 250000,
-      },
-      {
-        name: 'Potato Salad',
-        description: 'Why did you spend money on this?',
-        goal: 5,
-        earned: 100000000,
-      },
-    ];
-  }
+    return fetch('https://tiy-tn-class-api-fall-16.herokuapp.com/api/punch/projects')
+      .then((r) => r.json());
+  },
 });
